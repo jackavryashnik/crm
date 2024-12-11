05da3295ce9de34d130ed7461e2f65ad9e950191
jackavryashnik <jackavryashnik@gmail.com>
2024-12-11 23:56:47 +0000

    created status label component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import StatusLabel, { Status } from './components/StatusLabel';
-
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
