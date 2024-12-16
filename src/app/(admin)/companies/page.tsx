35667c08b6365f7303ab88e7cb2af856429109fb
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2024-12-16 17:13:26 +0200

    add layout

---
+++ src/app/(admin)/companies/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from '@/app/components/Header';
+import Toolbar from '@/app/components/Toolbar';
+import SearchInput from '@/app/components/SearchInput';
+import AddCompanyBtn from '@/app/components/AddCompanyBtn';
+import CompanyTable from '@/app/components/CompanyTable';
+import CompanyRow from '@/app/components/CompanyRow';
+import { Status } from '@/app/components/StatusLabel';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <>
+      <Header>Companies</Header>
+      <main>
+        <Toolbar action={<AddCompanyBtn />}>
+          <SearchInput />
+        </Toolbar>
+        <CompanyTable>
+          <CompanyRow
+            id={1}
+            category="Products"
+            company="Costco"
+            status={Status.Pending}
+            promotion={true}
+            country="USA"
+            joinedDate="02.19.2023"
+          />
+        </CompanyTable>
+      </main>
+    </>
+  );
+}
