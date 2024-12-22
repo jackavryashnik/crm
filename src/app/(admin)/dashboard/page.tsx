5178e5ca168825fe7997d9458746ad17c74b9b90
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2024-12-22 19:47:44 +0000

    add error handling

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,11 +1,11 @@
+import MagicBtn from '@/app/components/MagicBtn';
 import React from 'react';
 
-export interface PageProps {}
-
-export default function Page({}: PageProps) {
+export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
+      <MagicBtn />
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import AddCompanyBtn from './components/AddCompanyBtn';
+import MagicButton from './components/MagicBtn';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyBtn />
+      <MagicButton/>
     </main>
   );
 }
