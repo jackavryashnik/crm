54dbedc63536ae61634f5aafaa11be7593002fd3
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2024-12-23 20:50:50 +0000

    removed magic btn

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import MagicBtn from '@/app/components/MagicBtn';
 import React from 'react';
 
 export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
-      <MagicBtn />
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import AddCompanyBtn from './components/AddCompanyBtn';
-import MagicButton from './components/MagicBtn';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyBtn />
-      <MagicButton/>
     </main>
   );
 }
