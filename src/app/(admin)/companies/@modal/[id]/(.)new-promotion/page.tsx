dce548cfe4707731b63a830a7b51540a708328e9
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2025-01-06 21:26:39 +0000

    Update page.tsx

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import PromotionFormModal from '@/app/components/PromotionFormModal';
 
-export interface PageProps {
-  params: { id: string };
-}
-
-export default  function Page({ params }: PageProps) {
+export default function Page() {
   const router = useRouter();
-  const { id } = params;
+  const params = useParams();
+  const id = params?.id;
+
+  if (!id || Array.isArray(id)) {
+    return <div>Error: ID not found or invalid</div>;
+  }
+
   return (
     <PromotionFormModal
       companyId={id}
