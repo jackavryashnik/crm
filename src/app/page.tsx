806c03ea60cbc2d8a82c9326e38cc151432766a1
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2025-01-10 00:39:45 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import Button from './components/Button';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
+      <Button onClick={() => router.push('/dashboard')}>Open dashboard</Button>
     </main>
   );
 }
